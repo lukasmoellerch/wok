c2228487ef09eeb6faa2facba1cfadd6a0cb9b56
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-02 13:20:15 +0200

    Converted class into a collection of standalone functions

--- src/WASM/Encoding/Encoder.test.ts
+++ src/WASM/Encoding/Encoder.test.ts
@@ -1,5 +1,13 @@
 import { TypedArrayBytestreamConsumer } from "./TypedArrayBytestreamConsumer";
-import { Encoder } from "./Encoder";
+import {
+  encodeNumberAsSignedLEB128,
+  encodeNumberAsUnsignedLEB128,
+  encodeFloat32,
+  encodeFloat64,
+  encodeUInt8,
+  encodeSInt8,
+  encodeUTF8String
+} from "./Encoder";
 test("should encode signed numbers using LEB128", () => {
   const cases: Array<[number, number[]]> = [
     [0, [0]],
@@ -14,11 +22,10 @@ test("should encode signed numbers using LEB128", () => {
     [-128, [0x80, 0x7f]],
     [-129, [0xff, 0x7e]]
   ];
-  const encoder = new Encoder();
   for (let testCase of cases) {
     let [n, expected] = testCase;
     let consumer = new TypedArrayBytestreamConsumer();
-    encoder.encodeNumberAsSignedLEB128(n, consumer);
+    encodeNumberAsSignedLEB128(n, consumer);
     let result = consumer.cleanArray;
     let expectedArray = new Uint8Array(expected);
     expect(result).toEqual(expectedArray);
@@ -38,31 +45,28 @@ test("should encode unsigned numbers using LEB128", () => {
     [0x40c, [0x8c, 0x08]],
     [624485, [0xe5, 0x8e, 0x26]]
   ];
-  const encoder = new Encoder();
   for (let testCase of cases) {
     let [n, expected] = testCase;
     let consumer = new TypedArrayBytestreamConsumer();
-    encoder.encodeNumberAsUnsignedLEB128(n, consumer);
+    encodeNumberAsUnsignedLEB128(n, consumer);
     let result = consumer.cleanArray;
     let expectedArray = new Uint8Array(expected);
     expect(result).toEqual(expectedArray);
   }
 });
 test("should be able to encode 32-bit floats correctly", () => {
-  const encoder = new Encoder();
   const encode = (n: number) => {
     const buffer = new TypedArrayBytestreamConsumer();
-    encoder.encodeFloat32(n, buffer);
+    encodeFloat32(n, buffer);
     return [...buffer.cleanArray];
   };
   expect(encode(12)).toEqual([0x00, 0x00, 0x40, 0x41]);
   expect(encode(820)).toEqual([0x00, 0x00, 0x4d, 0x44]);
 });
 test("should be able to encode 64-bit floats correctly", () => {
-  const encoder = new Encoder();
   const encode = (n: number) => {
     const buffer = new TypedArrayBytestreamConsumer();
-    encoder.encodeFloat64(n, buffer);
+    encodeFloat64(n, buffer);
     return [...buffer.cleanArray];
   };
   expect(encode(200)).toEqual([0, 0, 0, 0, 0, 0, 105, 64]);
@@ -70,10 +74,9 @@ test("should be able to encode 64-bit floats correctly", () => {
   expect(encode(234.23412612)).toEqual([67, 145, 15, 246, 125, 71, 109, 64]);
 });
 test("should be able to encode 8-bit unsigned integers correctly", () => {
-  const encoder = new Encoder();
   const encode = (n: number) => {
     const buffer = new TypedArrayBytestreamConsumer();
-    encoder.encodeUInt8(n, buffer);
+    encodeUInt8(n, buffer);
     return [...buffer.cleanArray];
   };
   expect(encode(3)).toEqual([3]);
@@ -83,14 +86,50 @@ test("should be able to encode 8-bit unsigned integers correctly", () => {
 });
 
 test("should be able to encode 8-bit signed integers correctly", () => {
-  const encoder = new Encoder();
   const encode = (n: number) => {
     const buffer = new TypedArrayBytestreamConsumer();
-    encoder.encodeSInt8(n, buffer);
+    encodeSInt8(n, buffer);
     return [...buffer.cleanArray];
   };
   expect(encode(3)).toEqual([3]);
   expect(encode(149)).toEqual([149]);
   expect(encode(42)).toEqual([42]);
   expect(encode(-12)).toEqual([244]);
+});
+test("should be able to encode strings as utf-8", () => {
+  const encode = (s: string) => {
+    const buffer = new TypedArrayBytestreamConsumer();
+    encodeUTF8String(s, buffer);
+    return [...buffer.cleanArray];
+  };
+  expect(encode("Test")).toEqual([84, 101, 115, 116]);
+  expect(encode("Foo © bar 𝌆 baz ☃ qux")).toEqual([
+    0x46,
+    0x6f,
+    0x6f,
+    0x20,
+    0xc2,
+    0xa9,
+    0x20,
+    0x62,
+    0x61,
+    0x72,
+    0x20,
+    0xf0,
+    0x9d,
+    0x8c,
+    0x86,
+    0x20,
+    0x62,
+    0x61,
+    0x7a,
+    0x20,
+    0xe2,
+    0x98,
+    0x83,
+    0x20,
+    0x71,
+    0x75,
+    0x78
+  ]);
 });
--- src/WASM/Encoding/Encoder.ts
+++ src/WASM/Encoding/Encoder.ts
@@ -1,55 +1,75 @@
 import { IBytestreamConsumer } from "./IBytestreamConsumer";
-export class Encoder {
-  constructor() {}
-  encodeNumberAsSignedLEB128(n: number, consumer: IBytestreamConsumer): void {
-    const buffer = [];
+import { TextEncoder } from "util";
+const textEncoder =
+  typeof TextEncoder !== "undefined" ? new TextEncoder() : null;
+export function encodeNumberAsSignedLEB128(
+  n: number,
+  consumer: IBytestreamConsumer
+): void {
+  const buffer = [];
 
-    let more = true;
-    while (more) {
-      let byte = n & 0x7f;
-      n >>= 7;
-      if (n === 0 && (byte & 0x40) === 0) {
-        more = false;
-      } else if (n === -1 && byte & 0x40) {
-        more = false;
-      } else {
-        byte |= 0x80;
-      }
-      buffer.push(byte);
+  let more = true;
+  while (more) {
+    let byte = n & 0x7f;
+    n >>= 7;
+    if (n === 0 && (byte & 0x40) === 0) {
+      more = false;
+    } else if (n === -1 && byte & 0x40) {
+      more = false;
+    } else {
+      byte |= 0x80;
     }
+    buffer.push(byte);
+  }
 
-    consumer.write(buffer);
-  }
-  encodeNumberAsUnsignedLEB128(n: number, consumer: IBytestreamConsumer): void {
-    const buffer = [];
-    do {
-      let byte = n & 0x7f;
-      n >>>= 7;
-      if (n !== 0) {
-        byte |= 0x80;
-      }
-      buffer.push(byte);
-    } while (n !== 0);
-    for (let byte of buffer) {
-      consumer.write(byte);
+  consumer.write(buffer);
+}
+export function encodeNumberAsUnsignedLEB128(
+  n: number,
+  consumer: IBytestreamConsumer
+): void {
+  const buffer = [];
+  do {
+    let byte = n & 0x7f;
+    n >>>= 7;
+    if (n !== 0) {
+      byte |= 0x80;
     }
+    buffer.push(byte);
+  } while (n !== 0);
+  for (let byte of buffer) {
+    consumer.write(byte);
   }
-  encodeFloat32(n: number, consumer: IBytestreamConsumer): void {
-    const array = new Float32Array([n]);
-    const bytes = new Uint8Array(array.buffer);
-    consumer.write([...bytes], 4);
-  }
-  encodeFloat64(n: number, consumer: IBytestreamConsumer): void {
-    const array = new Float64Array([n]);
-    const bytes = new Uint8Array(array.buffer);
-    consumer.write([...bytes], 8);
-  }
-  encodeUInt8(n: number, consumer: IBytestreamConsumer): void {
-    consumer.write([n], 1);
-  }
-  encodeSInt8(n: number, consumer: IBytestreamConsumer): void {
-    const array = new Int8Array([n]);
-    const bytes = new Uint8Array(array.buffer);
-    consumer.write([...bytes], 1);
+}
+export function encodeFloat32(n: number, consumer: IBytestreamConsumer): void {
+  const array = new Float32Array([n]);
+  const bytes = new Uint8Array(array.buffer);
+  consumer.write([...bytes], 4);
+}
+export function encodeFloat64(n: number, consumer: IBytestreamConsumer): void {
+  const array = new Float64Array([n]);
+  const bytes = new Uint8Array(array.buffer);
+  consumer.write([...bytes], 8);
+}
+export function encodeUInt8(n: number, consumer: IBytestreamConsumer): void {
+  consumer.write([n], 1);
+}
+export function encodeSInt8(n: number, consumer: IBytestreamConsumer): void {
+  const array = new Int8Array([n]);
+  const bytes = new Uint8Array(array.buffer);
+  consumer.write([...bytes], 1);
+}
+export function encodeUTF8String(
+  string: string,
+  consumer: IBytestreamConsumer
+): void {
+  if (textEncoder) {
+    const array = textEncoder.encode(string);
+    consumer.write([...array]);
+  } else {
+    for (let i = 0; i < string.length; i++) {
+      const charCode = string.charCodeAt(i);
+      consumer.write(charCode);
+    }
   }
 }
