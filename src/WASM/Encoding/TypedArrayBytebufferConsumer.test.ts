6e8a11fae5c6618836316b67e68722ec10d9e120
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-04 14:37:57 +0200

    tslint

--- src/WASM/Encoding/TypedArrayBytebufferConsumer.test.ts
+++ src/WASM/Encoding/TypedArrayBytebufferConsumer.test.ts
@@ -1,7 +1,7 @@
 import { TypedArrayBytestreamConsumer } from "./TypedArrayBytestreamConsumer";
 
 test("Sequentially writing single bytes to the buffer", () => {
-  let consumer = new TypedArrayBytestreamConsumer();
+  const consumer = new TypedArrayBytestreamConsumer();
   consumer.write(42);
   consumer.write(23);
   consumer.write(89);
@@ -10,26 +10,26 @@ test("Sequentially writing single bytes to the buffer", () => {
   expect(result).toEqual(new Uint8Array([42, 23, 89, 42]));
 });
 test("Sequentially writing arrays of bytes to the buffer", () => {
-  let consumer = new TypedArrayBytestreamConsumer();
+  const consumer = new TypedArrayBytestreamConsumer();
   consumer.write([23, 64]);
   consumer.write([36, 12]);
   const result = consumer.cleanArray;
   expect(result).toEqual(new Uint8Array([23, 64, 36, 12]));
 });
 test("Zero padding arrays of bytes containing less bytes than the total size", () => {
-  let consumer = new TypedArrayBytestreamConsumer();
+  const consumer = new TypedArrayBytestreamConsumer();
   consumer.write([23, 64], 4);
   const result = consumer.cleanArray;
   expect(result).toEqual(new Uint8Array([23, 64, 0, 0]));
 });
 test("Truncating arrays of bytes containing more bytes than the size", () => {
-  let consumer = new TypedArrayBytestreamConsumer();
+  const consumer = new TypedArrayBytestreamConsumer();
   consumer.write([23, 64, 12, 42, 9, 8, 7, 6], 4);
   const result = consumer.cleanArray;
   expect(result).toEqual(new Uint8Array([23, 64, 12, 42]));
 });
 test("Single bytes can be written to reservde positions", () => {
-  let consumer = new TypedArrayBytestreamConsumer();
+  const consumer = new TypedArrayBytestreamConsumer();
   consumer.write(12);
   consumer.write(53);
   const position = consumer.reserveBytes(1);
@@ -39,7 +39,7 @@ test("Single bytes can be written to reservde positions", () => {
   expect(result).toEqual(new Uint8Array([12, 53, 52, 35]));
 });
 test("Reserving bytes enables writing to the buffer at that position later", () => {
-  let consumer = new TypedArrayBytestreamConsumer();
+  const consumer = new TypedArrayBytestreamConsumer();
   consumer.write(12);
   consumer.write(53);
   const position = consumer.reserveBytes(2);
@@ -49,7 +49,7 @@ test("Reserving bytes enables writing to the buffer at that position later", () 
   expect(result).toEqual(new Uint8Array([12, 53, 52, 54, 35]));
 });
 test("Setting size when writing an array with more elements than specified to a reserved position truncates the data", () => {
-  let consumer = new TypedArrayBytestreamConsumer();
+  const consumer = new TypedArrayBytestreamConsumer();
   consumer.write(12);
   consumer.write(53);
   const position = consumer.reserveBytes(2);
@@ -59,7 +59,7 @@ test("Setting size when writing an array with more elements than specified to a 
   expect(result).toEqual(new Uint8Array([12, 53, 52, 54, 35]));
 });
 test("Setting size when writing an array with less elements than specified to a reserved position fills the remaining bytes with zeroes", () => {
-  let consumer = new TypedArrayBytestreamConsumer();
+  const consumer = new TypedArrayBytestreamConsumer();
   consumer.write(12);
   consumer.write(53);
   const position = consumer.reserveBytes(2);
@@ -69,9 +69,9 @@ test("Setting size when writing an array with less elements than specified to a 
   expect(result).toEqual(new Uint8Array([12, 53, 52, 0, 35]));
 });
 test("Buffer is resized when writing many values to it", () => {
-  let consumer = new TypedArrayBytestreamConsumer(4, 2);
-  let bytes = 10000;
-  let typedArray = new Uint8Array(bytes);
+  const consumer = new TypedArrayBytestreamConsumer(4, 2);
+  const bytes = 10000;
+  const typedArray = new Uint8Array(bytes);
   for (let i = 0; i < bytes; i++) {
     typedArray[i] = i % 255;
     consumer.write(i % 255);
