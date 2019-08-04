358360386b7109657430d5e20a1cc1174a41ff7b
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-04 14:36:59 +0200

    tslint

--- src/WASM/Encoding/IBytestreamConsumer.ts
+++ src/WASM/Encoding/IBytestreamConsumer.ts
@@ -1,12 +1,10 @@
 export interface IBytestreamConsumer {
-  write(data: number): void;
-  write(data: number[]): void;
-  write(data: number[], size: number): void;
-  reserveBytes(amount: number): number;
-  writeAt(position: number, data: number): void;
-  writeAt(position: number, data: number[]): void;
-  writeAt(position: number, data: number[], size: number): void;
 
   cleanArray: Uint8Array;
+  write(data: number): void;
+  write(data: number[], size?: number): void;
+  reserveBytes(amount: number): number;
+  writeAt(position: number, data: number): void;
+  writeAt(position: number, data: number[], size?: number): void;
   append(buffer: IBytestreamConsumer): void;
 }
