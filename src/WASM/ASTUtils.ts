0008ada3f11859025748bbf3a904e3b77174d203
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-05 16:57:18 +0200

    Added utility function for comparing two function types.

---
+++ src/WASM/ASTUtils.ts
@@ -0,0 +1,20 @@
+import { IFunctionType } from "./AST";
+export function functionTypesAreEqual(lhs: IFunctionType, rhs: IFunctionType): boolean {
+  if (lhs.arguments.length !== rhs.arguments.length) {
+    return false;
+  }
+  if (rhs.results.length !== rhs.results.length) {
+    return false;
+  }
+  for (let i = 0; i < lhs.arguments.length; i++) {
+    if (lhs.arguments[i] !== rhs.arguments[i]) {
+      return false;
+    }
+  }
+  for (let i = 0; i < lhs.results.length; i++) {
+    if (lhs.results[i] !== rhs.results[i]) {
+      return false;
+    }
+  }
+  return true;
+}
