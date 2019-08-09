5502745fcb43c3fa276235fa2d2bd195bd657326
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-09 19:11:18 +0200

    Added AST node base class

---
+++ src/Frontend/AST/ASTNode.ts
@@ -0,0 +1,29 @@
+export interface IASTNode {
+  attributes: Map<keyof IAttributeMap, IAttribute>;
+  children: IASTNode[];
+}
+interface IAttributeMap {
+  type: ITypeAttribute;
+  name: INameAttribute;
+}
+interface IAttribute {
+  kind: keyof IAttributeMap;
+}
+interface ITypeAttribute extends IAttribute {
+  type: number;
+}
+interface INameAttribute extends IAttribute {
+  name: string;
+}
+
+export class ASTNode implements IASTNode {
+  public attributes: Map<keyof IAttributeMap, IAttribute> = new Map();
+  public children: IASTNode[] = [];
+  public getAttribute<T extends keyof IAttributeMap>(kind: T): IAttributeMap[T] {
+    const attribute = this.attributes.get(kind);
+    if (attribute === undefined) {
+      throw new Error("Attribute with specified name was not found");
+    }
+    return attribute as IAttributeMap[T];
+  }
+}
