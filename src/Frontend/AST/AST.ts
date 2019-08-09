8285b77edcf38707e8d0bddc7ce4ae2e3932798e
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-09 19:07:52 +0200

    Added LValue interface

--- src/Frontend/AST/AST.ts
+++ src/Frontend/AST/AST.ts
@@ -3,3 +3,6 @@ import { IASTNode } from "./ASTNode";
 export interface ITopLevelDeclaration extends IASTNode {
   topLevelDeclarable: void;
 }
+export interface ILValue extends IASTNode {
+  lvalue: void;
+}
