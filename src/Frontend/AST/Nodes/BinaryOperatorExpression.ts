6484d4e8f21b5fae02b3b463b83727e0d0b07deb
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-14 19:02:44 +0200

    Added AST node for binary operator expressions

---
+++ src/Frontend/AST/Nodes/BinaryOperatorExpression.ts
@@ -0,0 +1,13 @@
+import { Token } from "../../Lexer/Token";
+import { Expression } from "./Expression";
+export class BinaryOperatorExpression extends Expression {
+  public lhs: Expression;
+  public rhs: Expression;
+  public operator: Token;
+  constructor(lhs: Expression, rhs: Expression, operator: Token) {
+    super();
+    this.lhs = lhs;
+    this.rhs = rhs;
+    this.operator = operator;
+  }
+}
