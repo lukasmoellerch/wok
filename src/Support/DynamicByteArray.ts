b5535723f61aa1a5ffa5468661d42e6a4283c5d3
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-18 18:43:56 +0200

    Added dynamic byte array class

---
+++ src/Support/DynamicByteArray.ts
@@ -0,0 +1,66 @@
+export class DynamicByteArray {
+  public get reversed(): number[] {
+    const array: number[] = [];
+    for (let i = this.length - 1; i >= 0; i--) {
+      array.push(this.get(i));
+    }
+    return array;
+  }
+  public get array(): number[] {
+    const array: number[] = [];
+    for (let i = 0; i < this.length; i++) {
+      array.push(this.get(i));
+    }
+    return array;
+  }
+  private static nextPowerOfTwo(v: number) {
+    v--;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    v++;
+    return v;
+  }
+  public buffer: Uint8Array;
+  public length: number;
+  constructor(data: number[]) {
+    this.length = data.length;
+    const arraySize = DynamicByteArray.nextPowerOfTwo(this.length);
+    this.buffer = new Uint8Array(arraySize);
+    for (let i = 0; i < data.length; i++) {
+      this.buffer[i] = data[i];
+    }
+  }
+  public set(index: number, value: number) {
+    if (index >= this.buffer.length) {
+      const newBuffer = new Uint8Array();
+      newBuffer.set(this.buffer);
+      this.buffer = newBuffer;
+    }
+    if ((index + 1) > this.length) {
+      this.length = index + 1;
+    }
+    this.buffer[index] = value;
+  }
+  public get(index: number): number {
+    return this.buffer[index];
+  }
+  public map<T>(fn: ((arg: number) => T)): T[] {
+    const array: T[] = [];
+    for (let i = 0; i < this.length; i++) {
+      array.push(fn(this.get(i)));
+    }
+    return array;
+  }
+  public copy(): DynamicByteArray {
+    return new DynamicByteArray(this.array);
+  }
+  public unshift(value: number) {
+    for (let i = this.length; i > 0; i--) {
+      this.set(i, this.get(i - 1));
+    }
+    this.set(0, value);
+  }
+}
