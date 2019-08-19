7c4a0afeb5bc08e25f85196717129268897700a5
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-19 19:30:52 +0200

    Added new constructor

--- src/Support/DynamicByteArray.ts
+++ src/Support/DynamicByteArray.ts
@@ -25,17 +25,25 @@ export class DynamicByteArray {
   }
   public buffer: Uint8Array;
   public length: number;
-  constructor(data: number[]) {
-    this.length = data.length;
-    const arraySize = DynamicByteArray.nextPowerOfTwo(this.length);
-    this.buffer = new Uint8Array(arraySize);
-    for (let i = 0; i < data.length; i++) {
-      this.buffer[i] = data[i];
+  constructor(buffer: Uint8Array, length: number)
+  constructor(data: number[])
+  constructor(a: number[] | Uint8Array, b?: number) {
+    if (a instanceof Uint8Array && b !== undefined) {
+      this.buffer = a;
+      this.length = b;
+    } else {
+      const data = a;
+      this.length = data.length;
+      const arraySize = DynamicByteArray.nextPowerOfTwo(this.length);
+      this.buffer = new Uint8Array(arraySize);
+      for (let i = 0; i < data.length; i++) {
+        this.buffer[i] = data[i];
+      }
     }
   }
   public set(index: number, value: number) {
     if (index >= this.buffer.length) {
-      const newBuffer = new Uint8Array();
+      const newBuffer = new Uint8Array(DynamicByteArray.nextPowerOfTwo(index + 1));
       newBuffer.set(this.buffer);
       this.buffer = newBuffer;
     }
@@ -62,5 +70,23 @@ export class DynamicByteArray {
       this.set(i, this.get(i - 1));
     }
     this.set(0, value);
+  }
+  public splitAt(q: number): [DynamicByteArray, DynamicByteArray] {
+    const at = this.length - q;
+    const lowerHalfSize = DynamicByteArray.nextPowerOfTwo(at);
+    const upperHalfSize = DynamicByteArray.nextPowerOfTwo(this.length - at);
+    const lowerHalf = new Uint8Array(lowerHalfSize);
+    const upperHalf = new Uint8Array(upperHalfSize);
+    const l = new DynamicByteArray(lowerHalf, at);
+    const h = new DynamicByteArray(upperHalf, this.length - at);
+    for (let i = 0; i < at; i++) {
+      lowerHalf[i] = this.buffer[i];
+    }
+    let j = 0;
+    for (let i = at; i < this.length; i++) {
+      upperHalf[j] = this.buffer[i];
+      j++;
+    }
+    return [h, l];
   }
 }
