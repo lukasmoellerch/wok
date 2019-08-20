162bf62edc7ca0b5ea42c6417885624473d54dce
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-20 13:08:07 +0200

    Added scope attributes

--- src/Frontend/AST/Nodes/EpxressionWrapper.ts
+++ src/Frontend/AST/Nodes/EpxressionWrapper.ts
@@ -1,10 +1,15 @@
+import { OperatorScope } from "../../Expression Parsing/OperatorScopeBuilder";
 import { Token } from "../../Lexer/Token";
+import { TypeScope } from "../../Type Scope/TypeScopeBuilder";
 import { Expression } from "./Expression";
 import { Statement } from "./Statement";
 export class ExpressionWrapper extends Statement {
   public parsed: boolean = false;
   public tokens: Token[];
   public expression: Expression | undefined = undefined;
+
+  public typeScope: TypeScope = new TypeScope();
+  public operatorScope: OperatorScope = new OperatorScope();
   constructor(tokens: Token[]) {
     super();
     this.tokens = tokens;
