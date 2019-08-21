fd7e357453a7962a4f0b15bcd9fba1aadfb18e2c
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-21 10:56:02 +0200

    Fixed expression wrapper not updating children property

--- src/Frontend/AST/Nodes/ExpressionWrapper.ts
+++ src/Frontend/AST/Nodes/ExpressionWrapper.ts
@@ -1,15 +1,17 @@
 import { OperatorScope } from "../../Expression Parsing/OperatorScopeBuilder";
 import { Token } from "../../Lexer/Token";
 import { TypeScope } from "../../Type Scope/TypeScopeBuilder";
+import { AssignmentStatement } from "./AssignmentStatement";
 import { Expression } from "./Expression";
 import { Statement } from "./Statement";
 export class ExpressionWrapper extends Statement {
   public parsed: boolean = false;
   public tokens: Token[];
-  public expression: Expression | undefined = undefined;
+  public expression: Expression | AssignmentStatement | undefined = undefined;
 
   public typeScope: TypeScope = new TypeScope();
   public operatorScope: OperatorScope = new OperatorScope();
+  public raw: string;
 
   public allowAssignmentStatement: boolean;
   constructor(tokens: Token[], allowAssignmentStatement: boolean = false) {
@@ -17,5 +19,13 @@ export class ExpressionWrapper extends Statement {
     this.tokens = tokens;
     this.children = tokens;
     this.allowAssignmentStatement = allowAssignmentStatement;
+    const start = tokens[0].range.start.offset;
+    const end = tokens[tokens.length - 1].range.end.offset;
+    const source = tokens[0].range.sourceContent;
+    this.raw = source.substring(start, end);
+  }
+  public setExpression(expression: Expression | AssignmentStatement) {
+    this.children = [expression];
+    this.expression = expression;
   }
 }
