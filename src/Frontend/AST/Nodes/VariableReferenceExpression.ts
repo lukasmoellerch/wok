c1a95d45895aa316efd2492dcf24e12aadbfbe81
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-22 11:12:48 +0200

    Added resolveToEntry method

--- src/Frontend/AST/Nodes/VariableReferenceExpression.ts
+++ src/Frontend/AST/Nodes/VariableReferenceExpression.ts
@@ -1,5 +1,7 @@
 import { Token } from "../../Lexer/Token";
+import { VariableScopeEntry } from "../../VariableScope/VariableScope";
 import { ILValue } from "../AST";
+import { VariableScopeEntryAttribute } from "../Attributes/VariableScopeEntryAttribute";
 import { Expression } from "./Expression";
 
 export class VariableReferenceExpression extends Expression implements ILValue {
@@ -10,5 +12,9 @@ export class VariableReferenceExpression extends Expression implements ILValue {
     super();
     this.variableToken = variableToken;
     this.children = [variableToken];
+  }
+  public resolveToEntry(entry: VariableScopeEntry) {
+    this.entry = entry;
+    this.setAttribute(new VariableScopeEntryAttribute(entry));
   }
 }
