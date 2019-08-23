c5984b328c57370fdc33ed69e82c794adce2348d
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-23 13:39:49 +0200

    Impemented varaible scope builder

--- src/Frontend/VariableScope/VariableScopeBuilder.ts
+++ src/Frontend/VariableScope/VariableScopeBuilder.ts
@@ -7,7 +7,50 @@ import { UnboundFunctionDeclaration } from "../AST/Nodes/UnboundFunctionDeclarat
 import { VariableDeclaration } from "../AST/Nodes/VariableDeclaration";
 import { VariableReferenceExpression } from "../AST/Nodes/VariableReferenceExpression";
 import { ParserError, UndeclaredVariableUsageError } from "../Parser/ParserError";
-import { VariableScope, VariableScopeEntry, VariableScopeEntryType } from "./VariableScope";
+import { IType } from "../Type/Type";
+
+export enum VariableScopeEntryType {
+  globalUnboundFunction,
+  variable,
+  constant,
+  argument,
+}
+export type VariableScopeEntryDeclarationType = UnboundFunctionDeclaration | VariableDeclaration | ConstantDeclaration | FunctionArgumentDeclaration;
+let index = 0;
+export class VariableScopeEntry {
+  public index: number;
+  public str: string;
+  public entryType: VariableScopeEntryType;
+  public type: IType | undefined;
+  public declaration: VariableScopeEntryDeclarationType;
+  constructor(str: string, entryType: VariableScopeEntryType, declaration: VariableScopeEntryDeclarationType, type?: IType | undefined) {
+    this.str = str;
+    this.entryType = entryType;
+    this.type = type;
+    this.declaration = declaration;
+    this.index = index++;
+  }
+}
+export class VariableScope {
+  public parent: VariableScope | undefined;
+  public entryMap: Map<string, VariableScopeEntry> = new Map();
+  constructor(parent?: VariableScope | undefined) {
+    this.parent = parent;
+  }
+  public resolve(str: string): VariableScopeEntry | undefined {
+    const entry = this.entryMap.get(str);
+    if (entry !== undefined) {
+      return entry;
+    }
+    if (this.parent !== undefined) {
+      return this.parent.resolve(str);
+    }
+    return undefined;
+  }
+  public register(entry: VariableScopeEntry) {
+    this.entryMap.set(entry.str, entry);
+  }
+}
 export class VariableScopeBuilder extends ASTWalker {
   public sourceFile: SourceFile;
   public scopes: VariableScope[] = [];
@@ -43,7 +86,7 @@ export class VariableScopeBuilder extends ASTWalker {
     const str = variableDeclaration.identifierToken.content;
     const entryType = VariableScopeEntryType.variable;
     const type = variableDeclaration.typeHint;
-    const entry = new VariableScopeEntry(str, entryType, variableDeclaration, type);
+    const entry = new VariableScopeEntry(str, entryType, variableDeclaration, type !== undefined ? type.type : undefined);
     this.scopes[this.scopes.length - 1].register(entry);
     super.walkVariableDeclaration(variableDeclaration);
   }
@@ -51,7 +94,7 @@ export class VariableScopeBuilder extends ASTWalker {
     const str = constantDeclaration.identifierToken.content;
     const entryType = VariableScopeEntryType.variable;
     const type = constantDeclaration.typeHint;
-    const entry = new VariableScopeEntry(str, entryType, constantDeclaration, type);
+    const entry = new VariableScopeEntry(str, entryType, constantDeclaration, type !== undefined ? type.type : undefined);
     this.scopes[this.scopes.length - 1].register(entry);
     super.walkConstantDeclaration(constantDeclaration);
   }
@@ -59,7 +102,7 @@ export class VariableScopeBuilder extends ASTWalker {
     const str = argumentDeclaration.name.content;
     const entryType = VariableScopeEntryType.variable;
     const type = argumentDeclaration.type;
-    const entry = new VariableScopeEntry(str, entryType, argumentDeclaration, type);
+    const entry = new VariableScopeEntry(str, entryType, argumentDeclaration, type.type);
     this.scopes[this.scopes.length - 1].register(entry);
     return super.walkArgumentDeclaration(argumentDeclaration);
   }
