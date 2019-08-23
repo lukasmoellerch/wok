33576b9fdc4d7f0e48746ba9332722cc09a4654b
LukasM01 <lukas.moeller.d@gmail.com>
2019-08-23 18:24:40 +0200

    converted parameter to token instead of string

--- src/Frontend/Type/UnresolvedType/TypeMemberExpression.ts
+++ src/Frontend/Type/UnresolvedType/TypeMemberExpression.ts
@@ -1,14 +1,17 @@
+import { Token } from "../../Lexer/Token";
 import { TypeExpression } from "./TypeExpression";
 import { TypeReferenceExpression } from "./TypeReferenceExpression";
 export class TypeMemberExpression extends TypeExpression {
   public lhs: TypeReferenceExpression;
+  public member: Token;
   public parameters: TypeExpression[];
-  constructor(lhs: TypeReferenceExpression, parameters: TypeExpression[]) {
+  constructor(lhs: TypeReferenceExpression, member: Token, parameters: TypeExpression[]) {
     super();
     this.lhs = lhs;
+    this.member = member;
     this.parameters = parameters;
   }
   public stringifyType(): string {
-    return `${this.lhs.stringifyType()}<${this.parameters.map((a) => a.stringifyType()).join(", ")}>`;
+    return `${this.lhs.stringifyType()}${this.member.content}<${this.parameters.map((a) => a.stringifyType()).join(", ")}>`;
   }
 }
