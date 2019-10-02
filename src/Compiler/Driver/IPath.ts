26fbbb28819154cf88737346338de2ca2522e40a
lukasmoellerch <mail@lukas-moeller.ch>
2019-10-02 23:59:33 +0200

    Excluding lib in source file search

--- src/Compiler/Driver/IPath.ts
+++ src/Compiler/Driver/IPath.ts
@@ -1,3 +1,3 @@
-export class IPath {
-  
+export interface IPath {
+  parts: string[];
 }
--- src/Compiler/Driver/Loader.ts
+++ src/Compiler/Driver/Loader.ts
@@ -4,6 +4,7 @@ import { IPath } from "./IPath";
 export class Loader<FileSystemProvider extends IFileSystemProvider<Path, File>, Path extends IPath, File extends IFile> {
   private basePath: Path;
   private fileSystemProvider: FileSystemProvider;
+  private libPaths: Path[] = [];
   constructor(basePath: Path, fileSystemProvider: FileSystemProvider) {
     this.basePath = basePath;
     this.fileSystemProvider = fileSystemProvider;
@@ -15,11 +16,18 @@ export class Loader<FileSystemProvider extends IFileSystemProvider<Path, File>, 
     const content = await this.fileSystemProvider.getDirectoryContent(path);
     for await (const child of content) {
       if (child.type === "file") {
+        if (child.path.parts[child.path.parts.length - 1].indexOf("wok") === -1) {
+          continue;
+        }
         const file = await this.fileSystemProvider.readFile(child.path);
         if (file.type === "wok") {
           cb(file);
         }
       } else {
+        if (child.path.parts[child.path.parts.length - 1] === "lib") {
+          this.libPaths.push(child.path);
+          continue;
+        }
         await this.searchForSourceFiles(child.path, cb);
       }
     }
