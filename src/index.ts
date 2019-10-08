8b05162b2097dc53849395b902773b2bc60a0c2e
lukasmoellerch <mail@lukas-moeller.ch>
2019-10-08 12:53:50 +0200

    Moved error output to stderr

--- src/index.ts
+++ src/index.ts
@@ -98,7 +98,7 @@ export default async function main() {
   if (process.argv.includes("--print-ir")) {
     const irPrinter = new IRPrinter();
     const irString = irPrinter.stringifyCompilationUnit(compilationUnit);
-    process.stdout.write(irString);
+    process.stderr.write(irString);
   }
 
   const ssaTransformer = new SSATransformer();
