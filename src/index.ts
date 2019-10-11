0046628e634b72512b02945f941ade7fb400ff5a
lukasmoellerch <mail@lukas-moeller.ch>
2019-10-11 15:07:26 +0200

    Removed node dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import chalk from "chalk";
-import { readFile, writeFile } from "fs";
-import * as path from "path";
-import { promisify } from "util";
 import { DependencyAnalyzer } from "./Compiler/Frontend/DependencyAnalysis/DependencyAnalyzer";
 import { ErrorFormatter } from "./Compiler/Frontend/ErrorHandling/ErrorFormatter";
 import { ExpressionParser } from "./Compiler/Frontend/Expression Parsing/ExpressionParser";
 import { OperatorScope } from "./Compiler/Frontend/Expression Parsing/OperatorScope";
 import { OperatorScopeBuilder } from "./Compiler/Frontend/Expression Parsing/OperatorScopeBuilder";
 import { IRCompiler } from "./Compiler/Frontend/IRCompilation/Compiler";
-// import { IRCompiler } from "./Compiler/Frontend/IRCompilation/Compiler";
 import { Lexer } from "./Compiler/Frontend/Lexer/Lexer";
 import { Parser } from "./Compiler/Frontend/Parser/Parser";
 import { TypeResolver } from "./Compiler/Frontend/Type Scope/TypeResolver";
@@ -35,7 +30,7 @@ import { ErrorFormatter } from './Frontend/Parser/ErrorFormatter';
 import { Parser } from "./Frontend/Parser/Parser";
 import { BigUInt } from './Support/Math/BigUInt';
 */
-export default async function main() {
+export function compile(content: string, target: "wasm" | "java" | "ssa" | "ir"): string {
   /*const fileSystemProvider = new NodeNativeFileSystemProvider();
   const basePathString = path.resolve(process.argv[2] || "./").toString();
   const basePath = new Path(...basePathString.split("/"));
@@ -45,13 +40,10 @@ export default async function main() {
   });
   return;*/
 
-  const basePathString = path.resolve(process.argv[2] || "./example/main.wok").toString();
-
-  const content = await promisify(readFile)(basePathString || "testFile");
-  const lexer = new Lexer(basePathString, content.toString());
+  const lexer = new Lexer("main.wok", content);
   const parser = new Parser(lexer);
   const result = parser.parseSourceFile();
-  const errorFormatter = new ErrorFormatter(lexer.sourceString, basePathString, parser.errors);
+  const errorFormatter = new ErrorFormatter(lexer.sourceString, "main.wok", parser.errors);
 
   const globalOperatorScope = new OperatorScope();
   const globalTypeScope = new GlobalTypeTreeNode();
@@ -72,71 +64,53 @@ export default async function main() {
   operatorScopeBuilder.buildScopes();
   typeResolver.walkSourceFile(result);
   expressionParser.parseExpressions();
-  variableScopeBuilder.buildScopes();
+  variableScopeBuilder.buildScopes(globalVariableScope);
 
   const typeChecker = new TypeChecker(globalTypeScope, parser.errors);
   typeChecker.walkSourceFile(result);
   const implictConversionWrapper = new ImplictConversionWrapper();
   implictConversionWrapper.walkSourceFile(result);
-
-  if (process.argv.includes("--print-ast")) {
-    process.stdout.write(result.toString());
-  }
   if (parser.errors.length > 0) {
-    process.stdout.write(errorFormatter.toString());
-    return;
+    throw new Error(errorFormatter.toString());
   }
   const dependencyAnalyzer = new DependencyAnalyzer(parser.errors);
   dependencyAnalyzer.analyze(result, globalTypeScope.typeProvider);
-  if (process.argv.includes("--print-tasks")) {
-    for (const task of dependencyAnalyzer.compilerTasks) {
-      console.log(task.toString());
-    }
-  }
   const irCompiler = new IRCompiler(globalTypeScope, dependencyAnalyzer.compilerTasks);
   irCompiler.compile();
   const compilationUnit = irCompiler.compilationUnit;
-
-  if (process.argv.includes("--print-ir")) {
+  if (target === "ir") {
     const irPrinter = new IRPrinter();
-    const irString = irPrinter.stringifyCompilationUnit(compilationUnit);
-    process.stderr.write(irString);
+    return irPrinter.stringifyCompilationUnit(compilationUnit);
   }
-
   const ssaTransformer = new SSATransformer();
   const ssa = ssaTransformer.transformCompilationUnit(compilationUnit);
   if (true) { removeCopyStatements(ssa); }
   if (true) { removeUnused(ssa); }
 
-  if (process.argv.includes("--print-ssa")) {
+  if (target === "ssa") {
     const irPrinter = new IRPrinter();
-    const irString = irPrinter.stringifyCompilationUnit(ssa);
-    process.stdout.write(irString);
+    return irPrinter.stringifyCompilationUnit(ssa);
   }
-  const target = process.argv.includes("--java") ? "java" : "wasm";
+
   if (target === "wasm") {
     const wasmModule = WASMTarget.compileIR(ssa);
     const consumer = new TypedArrayBytestreamConsumer();
     encodeModule(wasmModule, consumer);
     const encoded = consumer.cleanArray;
-    const asyncWriteFile = promisify(writeFile);
-    await asyncWriteFile("./out.wasm", encoded);
     const module = new WebAssembly.Module(encoded);
     const decoder = new TextDecoder("utf-8");
     const instance = new WebAssembly.Instance(module, {
       env: {
         printInt(int: number) {
-          process.stdout.write(int.toString() + "\n");
         },
         print(ptr: number, length: number) {
           const view = new DataView(memory, ptr, length);
           const str = decoder.decode(view);
-          process.stdout.write(str);
         },
       },
     });
-    const memory = instance.exports.memory.buffer as ArrayBuffer;
-    instance.exports._start();
+    const memory = (instance.exports.memory as any).buffer as ArrayBuffer;
+    (instance.exports as any)._start();
   } else if (target === "java") {
     const javaSource = JavaTarget.compileIR(ssa, (name, java) => {
       if (name === "function_print") {
@@ -156,10 +130,9 @@ export default async function main() {
         java.writeLine("System.out.println(a0);");
       }
     });
-    process.stdout.write(javaSource);
+    return javaSource;
   } else {
-    process.stderr.write(chalk.red("Target was not defined.\n"));
+    throw new Error();
   }
-
+  throw new Error();
 }
-export const version = "0.0";
